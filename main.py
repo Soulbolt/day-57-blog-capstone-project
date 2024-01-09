fd9bfd01796a8cbca29a5de6c015a24d5bae4c25
Soulbolt <soulbolt@gmail.com>
2024-01-09 16:15:56 -0700

    Added blog api, modified html to use jinja template to display all blog posts.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+import requests
 
-
+BLOG_API_ENDPOINT = "https://api.npoint.io/c790b4d5cab58020d391"
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    response = requests.get(BLOG_API_ENDPOINT)
+    response.raise_for_status
+    all_post = response.json()
+    return render_template("index.html", posts=all_post)
 
 if __name__ == "__main__":
     app.run(debug=True)
