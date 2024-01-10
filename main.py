779e331920fca02254586dde3463045bbb7dc371
Soulbolt <soulbolt@gmail.com>
2024-01-09 17:40:41 -0700

    Added properties to the post class and used it to create a list of the posts and passed onto post.html to display blog based on the article id clicked on read from home page.

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from flask import Flask, render_template
 import requests
+from post import Post
 
-BLOG_API_ENDPOINT = "https://api.npoint.io/c790b4d5cab58020d391"
+get_post = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+post_objects = []
+
+for post in get_post:
+    post_content = Post(post["id"], post["title"], post["subtitle"], post["body"])
+    post_objects.append(post_content)
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    response = requests.get(BLOG_API_ENDPOINT)
-    response.raise_for_status
-    all_post = response.json()
-    return render_template("index.html", posts=all_post)
+    return render_template("index.html", posts=post_objects)
+
+@app.route('/post/<int:num>')
+def post(num):
+    return render_template("post.html", posts=post_objects, num=num)
 
 if __name__ == "__main__":
     app.run(debug=True)
